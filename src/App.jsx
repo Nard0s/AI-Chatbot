8ce5d1601ba6a7197135c531d835128927a05c48
yordanossole <yordanos.sole@gmail.com>
2025-07-15 15:21:43 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='text-center'>
+        <h1 className="text-3xl font-mono underline text-center">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
